c872c2e5215dae22f8bbe9adb1eba677f08e9d51
Justin Liao <jgliao248@gmail.com>
2025-01-28 09:19:52 -0500

    added dvorak layer and edited keymap

---
+++ config/arm/key-labels.h
@@ -0,0 +1,81 @@
+// ------------------------------------------------------------------------------------------
+// | k0   | k1   | k2   | k3   | k4   | k5   | k6   | k7   | k8   | k9   | k10  | k11  | k12  | k13  |   k14   |
+// | k15  | k16  | k17  | k18  | k19  | k20  | k21  | k22  | k23  | k24  | k25  | k26  | k27  |   k28   |
+// | k29  | k30  | k31  | k32  | k33  | k34  | k35  | k36  | k37  | k38  | k39  | k40  |     k41     |
+// |  k42        | k43  | k44  | k45  | k46  | k47  | k48  | k49  | k50  | k51  | k52  | k53  | k54   | k55 |
+// |  k56  | k57  | k58  |            k59               | k60  | k61  | k62  | k63  | k64  |
+// ------------------------------------------------------------------------------------------
+
+
+#pragma once
+
+#define K0  0
+#define K1  1
+#define K2  2
+#define K3  3
+#define K4  4
+#define K5  5
+#define K6  6
+#define K7  7
+#define K8  8
+#define K9  9
+#define K10 10
+#define K11 11
+#define K12 12
+#define K13 13
+#define K14 14  
+
+#define K15 15
+#define K16 16
+#define K17 17
+#define K18 18
+#define K19 19
+#define K20 20
+#define K21 21
+#define K22 22
+#define K23 23
+#define K24 24
+#define K25 25
+#define K26 26
+#define K27 27
+#define K28 28
+
+#define K29 29
+#define K30 30
+#define K31 31
+#define K32 32
+#define K33 33
+#define K34 34
+#define K35 35
+#define K36 36
+#define K37 37
+#define K38 38
+#define K39 39
+#define K40 40
+#define K41 41
+
+#define K42 42
+#define K43 43
+#define K44 44
+#define K45 45
+#define K46 46
+#define K47 47
+#define K48 48
+#define K49 49
+#define K50 50
+#define K51 51
+#define K52 52
+#define K53 53
+#define K54 54
+#define K55 55
+
+#define K56 56
+#define K57 57
+#define K58 58
+#define K59 59
+#define K60 60
+#define K61 61
+#define K62 62
+#define K63 63
+#define K64 64 
+
